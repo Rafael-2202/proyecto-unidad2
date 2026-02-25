04aed90f26160f37b78bbfcc75c0632df72c602e
Rafael <rafeldelacruztorres@gmail.com>
2026-02-24 21:57:26 -0600

    se agregó la función de cambiar imagenes al final de la pagina

--- java.js
+++ java.js
@@ -57,6 +57,19 @@
             }
         });
 
+
+        const mostrar = document.getElementById("mostrar");
+        const regalo = document.getElementById("regalo");
+
+        mostrar.addEventListener("click", function(){
+            if (regalo.style.display === "none"){
+                regalo.style.display = "flex";
+                document.getElementById("mostrar").innerText = "ocultar";
+            }else{
+                regalo.style.display = "none";
+                document.getElementById("mostrar").innerText = "Click Aquí";
+            }
+        })
 //contador-------------------------------------------------------------------------------
         const compra = document.querySelectorAll(".compra");
         const contador = document.getElementById("contador");
@@ -88,3 +101,18 @@
                 mensaje.style.color = "green"
             }
         });
+
+//Cambiar imagen-------------------------------------------------------------------------------
+        const img = document.getElementById("cambio_imagen");
+        let imagenes = ["https://i.pinimg.com/736x/81/ff/cc/81ffcc3bb8c55b83383e4e1b26c74965.jpg", "https://i.pinimg.com/736x/81/ff/cc/81ffcc3bb8c55b83383e4e1b26c74965.jpg", "https://i.pinimg.com/736x/17/ac/00/17ac00e2fb2e64354edb47400f0ed33f.jpg", "https://i.pinimg.com/1200x/8d/7b/92/8d7b92ac0eb1b485004c25c1101487eb.jpg", "https://i.pinimg.com/736x/fe/31/02/fe3102b9eb836d9bb527a5fa3a6612c4.jpg", "https://i.pinimg.com/736x/79/11/d6/7911d66305f80967a108c3a68b1bba61.jpg"]
+        
+        let j = 0;
+
+        function cambiarImagen(){
+            j++
+            if(j >= imagenes.length){
+                j = 0;
+            }
+            document.getElementById("regalito").src = imagenes[j]
+        }
+        img.addEventListener("click", cambiarImagen);
